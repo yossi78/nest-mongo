0ccc475693492dde97aa29e6dc5681e306f51a57
yossi78 <yossi78@gmail.com>
2024-12-24 17:30:54 +0200

    refactor main.ts to use a logger instance for improved logging during application bootstrap

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,13 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap'); // Create a logger instance
   const app = await NestFactory.create(AppModule);
-  app.useLogger(app.get(Logger));
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useLogger(logger); // Use the logger instance
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+
+  logger.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
