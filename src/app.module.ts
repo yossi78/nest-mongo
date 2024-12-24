58ad695f7e27b4ec12ac1772d4b7318e0682aa9a
yossi78 <yossi78@gmail.com>
2024-12-24 18:11:32 +0200

    feat: add @nestjs/config for environment variable management and update dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,30 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { LoggerMiddleware } from './middleware/logger.middleware';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/nest'), UserModule],
+  imports: [
+    // Load environment variables
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes ConfigModule available globally
+    }),
+    // Setup Mongoose with environment-based configuration
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: `mongodb://${configService.get('MONGO_USER')}:${configService.get('MONGO_PASSWORD')}@${configService.get('MONGO_HOST')}:${configService.get('MONGO_PORT')}`,
+        dbName: configService.get('MONGO_DB'),
+        authSource: 'admin', // Use 'admin' authentication database
+      }),
+    }),
+    UserModule,
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(LoggerMiddleware).forRoutes('*');
+    consumer.apply(LoggerMiddleware).forRoutes('*'); // Apply LoggerMiddleware globally
   }
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Post, Body, Param, Delete, Put, HttpException, HttpStatus } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserEntity } from './entities/user.entity';
+import { Logger } from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
+  private readonly logger = new Logger(UserController.name);
   constructor(private readonly userService: UserService) {}
 
   @Post()
@@ -11,6 +13,7 @@ export class UserController {
     try {
       return await this.userService.create(createUserDto);
     } catch (error) {
+      this.logger.log('ERROR WHILE ADDING USER'+ error.stack);
       throw new HttpException('Error creating user', HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
