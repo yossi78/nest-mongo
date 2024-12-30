a482ac4bb025c9ed34285fe8815195e379510456
yossi78 <yossi78@gmail.com>
2024-12-30 13:32:11 +0200

    feat: enhance logging in LoggerMiddleware to include MongoDB configuration details

--- src/middleware/logger.middleware.ts
+++ src/middleware/logger.middleware.ts
@@ -1,10 +1,23 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware,Logger } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
+import { ConfigService } from '@nestjs/config';
+
+
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
+  constructor(private readonly configService: ConfigService) {}
+    private readonly logger = new Logger(LoggerMiddleware.name);
+
   use(req: Request, res: Response, next: NextFunction) {
     console.log(`[${new Date().toISOString()}] ${req.method} ${req.originalUrl}`);
+
+    console.log(`MONGO_DB = ${this.configService.get('MONGO_DB')}`);
+    console.log(`MONGO_PORT = ${this.configService.get('MONGO_PORT')}`);
+
+    this.logger.log(`MONGO_DB = ${this.configService.get('MONGO_USER')}`);
+    this.logger.log(`MONGO_PORT = ${this.configService.get('MONGO_PASSWORD')}`);
     next();
+
   }
 }
